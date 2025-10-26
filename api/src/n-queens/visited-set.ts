23f595699d2a3f7fd6ca1600b4d656fc775e49a6
mei <mail@meihapps.gay>
2025-10-26 17:13:45 +0000

    implemented visited-set.ts

--- api/src/n-queens/visited-set.ts
+++ api/src/n-queens/visited-set.ts
@@ -5,7 +5,7 @@ import { BoardState } from "./types.js";
  * Returns true if the state exists in the visited set.
  */
 export function wasPreviouslyComputed(boardState: BoardState, visitedSet: Set<string>): boolean {
-  throw new Error("wasPreviouslyComputed not implemented");
+  return visitedSet.has(boardState.toString())
 }
 
 /**
@@ -13,5 +13,5 @@ export function wasPreviouslyComputed(boardState: BoardState, visitedSet: Set<st
  * The board state is serialized to a string for storage.
  */
 export function recordExploredState(boardState: BoardState, visitedSet: Set<string>): void {
-  throw new Error("recordExploredState not implemented");
+  visitedSet.add(boardState.toString())
 }
