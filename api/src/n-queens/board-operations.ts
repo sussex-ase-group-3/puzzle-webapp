77c1a25270935e109ea834e18456679dd45ae4e0
tb1819x <109041922+tb1819x@users.noreply.github.com>
2025-10-26 09:42:39 +0000

    Board operations is now completed passing all tests

--- api/src/n-queens/board-operations.ts
+++ api/src/n-queens/board-operations.ts
@@ -2,8 +2,13 @@ import { BoardState, Position } from "./types.js";
 import  { cloneDeep, size }  from 'lodash';
 
 /**
- * Places a queen at the specified position on the board.
- * Returns a new board state without mutating the original.
+ * Function purpose: Places a queen at a specified position on the board.
+ * Creates a deep copy of the board to ensure the original state is not
+ * mutated.
+ * @param {BoardState} boardState, current board state, idx is row & val is col
+ * @param {Position} newPosition A read-only tuple `[row, column]`.
+ * @returns {BoardState} A new board state with the queen placed.
+ * @throws {Error} For out of bounds and if queen already exist in position
  */
 export function placeQueen(boardState: BoardState, newPosition: Position): BoardState {
   
@@ -26,11 +31,11 @@ export function placeQueen(boardState: BoardState, newPosition: Position): Board
 }
 
 /**
- * Checks if placing a queen at the specified position would be safe
+ * Function purpose: Checks if placing a queen at the specified position would be safe
  * (no conflicts with existing queens).
- * check how every queen position compares to current position against current queen
- * the distance up and across between queens must be the same distance for it to be safe
- * the difference between col indexes and row index must not be the same
+ * @param {BoardState} boardState, current board state, idx is row & val is col
+ * @param {Position} newPosition A read-only tuple `[row, column]`.
+ * @returns {boolean} A boolean value true for safe otherwise false.
  */
 export function isSafe(boardState: BoardState, newPosition: Position): boolean {
 
@@ -58,8 +63,10 @@ export function isSafe(boardState: BoardState, newPosition: Position): boolean {
 }
 
 /**
- * Checks if the board state represents a complete solution
+ * Function purpose: Checks if the board state represents a complete solution
  * (all N queens are placed).
+ * @param {BoardState} boardState, current board state, idx is row & val is col
+ * @returns {boolean} A boolean value true for complete otherwise false.
  */
 export function isComplete(boardState: BoardState): boolean {
 
@@ -80,15 +87,17 @@ export function isComplete(boardState: BoardState): boolean {
 }
 
 /**
- * Returns a new board state rotated 90 degrees clockwise.
- * swap row and column first
- * formula n - 1 - row
+ * Function purpose: To rotate board 90 degrees clockwise.
+ * function swaps idx with col for new row, formula (n-1) - row for new col 
+ * 
+ * @param {BoardState} boardState, current board state, idx is row & val is col
+ * @returns {BoardState} Returns a new board state rotated 90 degrees clockwise.
  */
 export function rotateBoard(boardState: BoardState): BoardState {
   
   const currentBoardState = cloneDeep(boardState);
   const n = size(currentBoardState);
-  const newBoard = cloneDeep(currentBoardState)
+  const newBoard = cloneDeep(currentBoardState);
 
   currentBoardState.forEach((currentColumn, currentRow) => {
 
@@ -103,8 +112,27 @@ export function rotateBoard(boardState: BoardState): BoardState {
 }
 
 /**
- * Returns a new board state flipped horizontally.
+ * Function purpose: To flip current board horizontally.
+ * row value stays the same, new column is calculated using formyla (n-1)-col
+ * 
+ * @param {BoardState} boardState, current board state, idx is row & val is col
+ * @returns {BoardState} Returns a new board state flipped horizontally.
  */
 export function flipBoard(boardState: BoardState): BoardState {
-  throw new Error("flipBoard not implemented");
+
+  const currentBoardState = cloneDeep(boardState);
+  const newBoard = cloneDeep(boardState);
+  const n = size(currentBoardState);
+
+  currentBoardState.forEach((currentColumn, currentRow) => {
+
+     if (currentColumn !== -1) {
+        const newRow = cloneDeep(currentRow)
+        const col = cloneDeep(currentColumn)
+        newBoard[newRow] = ((n - 1) - col)
+     }    
+
+  });
+  return newBoard
+
 }
