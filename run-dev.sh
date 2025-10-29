d3475a84de4597a45734feb0130953407492d63a
mei happs <123597774+meihapps@users.noreply.github.com>
2025-10-29 16:52:57 +0000

    Delete run-dev.sh
    it's redundant now as the github action does what it was doing

--- run-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-(set -e; cd api && npm install && npm run dev) &
-API_PID=$!
-(set -e; cd web && npm install && npm run dev) &
-WEB_PID=$!
-trap "kill $API_PID $WEB_PID 2>/dev/null" EXIT
-wait
