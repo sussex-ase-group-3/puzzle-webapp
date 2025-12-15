da09702a7b17a63e141c80e85e20fd935856681a
mei <mail@meihapps.gay>
2025-12-15 13:15:18 +0000

    removed silly files

--- api/src/polysphere-dancing-links-3d/solver_3d.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-import { solveDancingLinks } from "../polysphere-dancing-links/dancing_links_solver.js";
-
-export type PyramidBoard = number[][][]; // [layer][row][col]
-
-export type Poly3DPuzzleState = {
